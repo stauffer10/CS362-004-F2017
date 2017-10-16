b87b623ca47ee20b94505a920aa62b5a6daf8f2d
Nathan Stauffer <nathanstauffer@gmail.com>
2017-10-16 17:19:33 -0400

    Add files via upload

--- projects/stauffen/dominion/unittest1.c
+++ projects/stauffen/dominion/unittest1.c
@@ -1,15 +1,25 @@
 //unittest1 - tests the initializeGame function of dominion.c
 
 #include "dominion.h"
-#include "test_helpers.h"
+#include "dominion_helpers.h"
 #include "rngs.h"
 #include "testHelpers.h"
 
 
 int main(){
 
-	
+	int result, seed = 42;
+	struct gameState *G;
+	int i, numberOfPlayers, 
+	int *kCards;
 
+	//set up attributes for a game
+	kCards = kingdomCards(8, 10, 12, 14, 16, 18, 20, 22, 24, 26); 	 //10 kingdom cards
+	G = newGame();							//game state
+	numberOfPlayers = 2;						//numPlayers
+	
+	//call initializeGame
+		
 
 
 
