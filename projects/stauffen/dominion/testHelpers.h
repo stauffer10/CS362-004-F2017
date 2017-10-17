b397a5dd0a0d444631e88b37d2a06ef5e7297a2e
Nathan Stauffer <nathanstauffer@gmail.com>
2017-10-17 08:49:46 -0400

    Update testHelpers.h

--- projects/stauffen/dominion/testHelpers.h
+++ projects/stauffen/dominion/testHelpers.h
@@ -8,6 +8,6 @@
 int intAssert(int received, int expected);
 void compareKC(struct gameState *G1, struct gameState *G2);
 void compareVC(struct gameState *G1, struct gameState *G2);
-void comparePlayerGameStates(int player, struct gameState *G1, struct gameState *G2);
+void comparePlayerDeckStates(int player, struct gameState *G1, struct gameState *G2);
 
 #endif
