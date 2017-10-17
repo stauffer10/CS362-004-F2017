8bdcec6bf5ab2994524f93ce2d7a1007a544c0e3
Nathan Stauffer <nathanstauffer@gmail.com>
2017-10-17 08:42:49 -0400

    Update testHelpers.h

--- projects/stauffen/dominion/testHelpers.h
+++ projects/stauffen/dominion/testHelpers.h
@@ -5,7 +5,7 @@
 #ifndef TESTHELPERS.H
 #define TESTHELPERS.H
 
-int intAssert(int a, int b);
+int intAssert(int received, int expected);
 void compareKC(struct gameState *G1, struct gameState *G2);
 void compareVC(struct gameState *G1, struct gameState *G2);
 void comparePlayerGameStates(int player, struct gameState *G1, struct gameState *G2);
