ece6fc5761001b50a611b6c390173cd987446144
Nathan Stauffer <nathanstauffer@gmail.com>
2017-10-17 10:29:34 -0400

    Add files via upload

--- projects/stauffen/dominion/unittest2.c
+++ projects/stauffen/dominion/unittest2.c
@@ -9,7 +9,32 @@
 
 int main(){
 
+	//establish variables
+
+
+	//initialize generic game
+
+
+	//copy second gameState from first
+
+
+	//call fullDeckCount for first player
+
+
+	//Does first player have correct number of each card to start game?
+
+
+	//add a smithy to deck, subtract 4 copper
+
+
+	//call fullDeckCount for first player again
+
+
+	//Is fullDeckCount 3 less than the last call?
+
 	
+	//Were any non-deck related attributes of gameState affected by call to fullDeckCount? 
+	 
 
 	return 0;
 }
--- projects/stauffen/dominion/unittest3.c
+++ projects/stauffen/dominion/unittest3.c
@@ -9,7 +9,60 @@
 
 int main(){
 
+	//set variables
+
+
+	//initialize 2 player game
+
+
+	//copy first gameState to second
+
+
+	//call endTurn
+
+
+	//check overall success or failure
+
+
+	//did whoseTurn increase?
+
+
+	//is player 1's hand empty?
+
+
+	//is player 1's discard 5?
+
+
+	//are numBuys and numActions reset to 1?
+
+
+	//are coins and playedCardCount reset to 0?
+
+
+	//copy first gameState to second again
+
+
+	//call endTurn again
+
+
+	//did whoseTurn reset to player 1?
+
 	
+	//is player 2's hand empty?
+
+
+	//is player 2's discard 5?
+
+	
+	//set 3 kingdom card piles to 0 (game over)
+
+	
+	//call endTurn
+
+
+	//did whoseTurn advance to player 2 (it shouldn't according to dominion.h)?
+
+
 
 	return 0;
 }
