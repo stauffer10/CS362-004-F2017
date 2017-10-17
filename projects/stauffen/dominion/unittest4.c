2038124067be7e690170416ede2d4e81eb859a6d
Nathan Stauffer <nathanstauffer@gmail.com>
2017-10-17 11:03:11 -0400

    Add files via upload

--- projects/stauffen/dominion/unittest4.c
+++ projects/stauffen/dominion/unittest4.c
@@ -9,7 +9,66 @@
 
 int main(){
 
-	
+	//set attributes - including a makeshift supply count variable
 
+
+	//initialize game that includes smithy but not adventurer
+
+
+	//copy second gameState from first
+
+
+	//call gainCard with smithy and toFlag=1, decrease supply count
+
+
+	//check overall pass (return 0)
+
+
+	//does gameState->smithyCount = supply count?
+
+
+	//do victory cards remain unchanged?
+
+
+	//are other gameState attributes (numBuys, numActions, whoseTurn, etc) unchanged?
+
+
+	//are all other kingdom card piles unchanged?
+
+
+	//are deck and deckCount increased by 1?
+
+
+	//copy second gameState from first again
+
+
+	//call gainCard with smithy and toFlag=2, decrease supply count
+
+
+	//does gameState->smithyCount = supply count?
+
+
+	//are hand and handCount increased by 1?
+
+
+	//copy second gameState from first again
+
+
+	//call gainCard with smithy and toFlag=3, decrease supply count
+
+
+	//does gameState->smithyCount = supply count?
+
+
+	//are discard and discardCount increased by 1?
+
+
+	//try to call gainCard for adventurer
+
+
+	//did call succeed (it shouldn't)?
+
+
+	
 	return 0;
 }
