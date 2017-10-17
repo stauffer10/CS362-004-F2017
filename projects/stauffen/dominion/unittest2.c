8c94ef002f76bc3ea4f6cc6e901bd419ee02127b
Nathan Stauffer <nathanstauffer@gmail.com>
2017-10-17 09:36:39 -0400

    Add files via upload

---
+++ projects/stauffen/dominion/unittest2.c
@@ -0,0 +1,15 @@
+//unittest2 - tests the fullDeckCount function of dominion.c
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include "testHelpers.h"
+#include <stdio>
+
+
+int main(){
+
+	
+
+	return 0;
+}
---
+++ projects/stauffen/dominion/unittest4.c
@@ -0,0 +1,15 @@
+//unittest4 - tests the gainCard function of dominion.c
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include "testHelpers.h"
+#include <stdio>
+
+
+int main(){
+
+	
+
+	return 0;
+}
