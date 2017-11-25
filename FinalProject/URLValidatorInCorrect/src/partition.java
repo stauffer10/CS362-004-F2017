4cc458aac3f3eec23472f501f3af466ed950ed15
Nathan Stauffer <nathanstauffer@gmail.com>
2017-11-25 03:31:14 -0500

    Add files via upload

---
+++ FinalProject/URLValidatorInCorrect/src/partition.java
@@ -0,0 +1,89 @@
+
+
+
+
+
+
+public void testYourFirstPartition(){
+
+	UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+
+	/****
+	* consider the five parts of URL - scheme, authority, port, path, query as
+	* [valid/invalid/absent, valid/invalid/absent, valid/invalid/absent, valid/invalid/absent, valid/invalid/absent]
+	* an example assertion may be [valid, valid, valid, valid, invalid]
+	****/
+
+	//first try valid URL's
+	//[valid, valid, valid, valid, valid]
+	assertTrue(urlVal.isValid("http://www.google.com:65535/test1?action=view"));
+	assertTrue(urlVal.isValid("http://afterzmusic.com:80/about/?action=edit&mode=up")); 	
+
+	//[absent, valid, valid, valid, valid]
+	assertTrue(urlVal.isValid("www.google.com:65535/test1?action=view"));
+	assertTrue(urlVal.isValid("afterzmusic.com:80/about/?action=edit&mode=up")); 
+
+	//[valid, valid, absent, valid, valid]
+	assertTrue(urlVal.isValid("http://www.google.com/test1?action=view"));
+	assertTrue(urlVal.isValid("http://afterzmusic.com/about/?action=edit&mode=up")); 
+
+	//[valid, valid, valid, absent, valid]
+	assertTrue(urlVal.isValid("http://www.google.com:65535?action=view"));
+	assertTrue(urlVal.isValid("http://afterzmusic.com:80?action=edit&mode=up")); 
+
+	//[valid, valid, valid, valid, absent]
+	assertTrue(urlVal.isValid("http://www.google.com:65535/test1"));
+	assertTrue(urlVal.isValid("http://afterzmusic.com:80/about/")); 
+
+	//[absent, valid, absent, valid, absent]
+	assertTrue(urlVal.isValid("www.google.com/test1"));
+	assertTrue(urlVal.isValid("afterzmusic.com/about/")); 
+
+	//[valid, valid, absent, absent, absent]
+	assertTrue(urlVal.isValid("http://www.google.com"));
+	assertTrue(urlVal.isValid("http://afterzmusic.com")); 
+
+	//try invalid URL's by testing one invalid section at a time
+	//[invalid, valid, valid, valid, valid]
+	assertFalse(urlVal.isValid("http:www.google.com:65535/test1?action=view"));
+	assertFalse(urlVal.isValid("://afterzmusic.com:80/about/?action=edit&mode=up")); 	
+
+	//[valid, invalid, valid, valid, valid]
+	assertFalse(urlVal.isValid("http://1.2.3:65535/test1?action=view"));
+	assertFalse(urlVal.isValid("http://afterz:80/about/?action=edit&mode=up")); 
+
+	//[valid, valid, invalid, valid, valid]
+	assertFalse(urlVal.isValid("http://www.google.com:-1/test1?action=view"));
+	assertFalse(urlVal.isValid("http://afterzmusic.com:ZZ/about/?action=edit&mode=up")); 
+
+	//[valid, valid, valid, invalid, valid]
+	assertFalse(urlVal.isValid("http://www.google.com:65535/../?action=view"));
+	assertFalse(urlVal.isValid("http://afterzmusic.com:80/about//file?action=edit&mode=up")); 
+
+	//[valid, valid, valid, valid, invalid]
+	assertFalse(urlVal.isValid("http://www.google.com:65535/test1?action=&"));
+	assertFalse(urlVal.isValid("http://afterzmusic.com:80/about/?edit&mode=up")); 
+
+	//[absent, invalid, absent, absent, absent]
+	assertFalse(urlVal.isValid("1.2.3"));
+	assertFalse(urlVal.isValid("afterz")); 
+	
+	//[absent, valid, absent, invalid, absent]
+	assertFalse(urlVal.isValid("www.google.com/../"));
+	assertFalse(urlVal.isValid("afterzmusic.com/about//file")); 
+
+	//[valid, absent, valid, valid, valid]
+	assertFalse(urlVal.isValid("http://:65535/test1?action=view"));
+	assertFalse(urlVal.isValid("http://:80/about/?action=edit&mode=up")); 
+
+}
+
+
+
+
+
+
+
+
+
+
